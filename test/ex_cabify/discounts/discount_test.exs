92a5809cddf6d506708001e06c6cbbb3c6af2795
Kamil Lelonek <squixy.sln@gmail.com>
2018-04-12 21:51:07 +0200

    Test Discount application

--- test/ex_cabify/discounts/discount_test.exs
+++ test/ex_cabify/discounts/discount_test.exs
@@ -5,60 +5,91 @@ defmodule ExCabify.DiscountTest do
 
   alias ExCabify.Discounts.{TwoForOne, Bulk}
 
-  test "should not apply a Discount for an empty Basket" do
-    refute Discounts.applies?(%Basket{}, Bulk)
-    refute(Discounts.applies?(%Basket{}, TwoForOne))
+  describe "applies?/2" do
+    test "should not apply a Discount for an empty Basket" do
+      refute Discounts.applies?(%Basket{}, Bulk)
+      refute(Discounts.applies?(%Basket{}, TwoForOne))
+    end
+
+    test "should not apply a Discount for not enough products" do
+      refute Discounts.applies?(%Basket{products: [%Product{code: "TSHIRT"}]}, Bulk)
+      refute(Discounts.applies?(%Basket{products: [%Product{code: "VOUCHER"}]}, TwoForOne))
+    end
+
+    test "should not apply a Discount for not specific products" do
+      refute Discounts.applies?(
+               %Basket{
+                 products: [
+                   %Product{code: "VOUCHER"},
+                   %Product{code: "VOUCHER"},
+                   %Product{code: "VOUCHER"}
+                 ]
+               },
+               Bulk
+             )
+
+      refute Discounts.applies?(
+               %Basket{
+                 products: [
+                   %Product{code: "TSHIRT"},
+                   %Product{code: "TSHIRT"},
+                   %Product{code: "TSHIRT"}
+                 ]
+               },
+               TwoForOne
+             )
+    end
+
+    test "should apply a Discount" do
+      assert Discounts.applies?(
+               %Basket{
+                 products: [
+                   %Product{code: "TSHIRT"},
+                   %Product{code: "TSHIRT"},
+                   %Product{code: "TSHIRT"}
+                 ]
+               },
+               Bulk
+             )
+
+      assert Discounts.applies?(
+               %Basket{
+                 products: [
+                   %Product{code: "VOUCHER"},
+                   %Product{code: "VOUCHER"}
+                 ]
+               },
+               TwoForOne
+             )
+    end
   end
 
-  test "should not apply a Discount for not enough products" do
-    refute Discounts.applies?(%Basket{products: [%Product{code: "TSHIRT"}]}, Bulk)
-    refute(Discounts.applies?(%Basket{products: [%Product{code: "VOUCHER"}]}, TwoForOne))
-  end
+  describe "apply/2" do
+    test "should apply Bulk discount" do
+      assert 19 * 3 ==
+               Discounts.apply(
+                 %Basket{
+                   products: [
+                     %Product{code: "TSHIRT"},
+                     %Product{code: "TSHIRT"},
+                     %Product{code: "TSHIRT"}
+                   ]
+                 },
+                 Bulk
+               )
+    end
 
-  test "should not apply a Discount for not specific products" do
-    refute Discounts.applies?(
-             %Basket{
-               products: [
-                 %Product{code: "VOUCHER"},
-                 %Product{code: "VOUCHER"},
-                 %Product{code: "VOUCHER"}
-               ]
-             },
-             Bulk
-           )
-
-    refute Discounts.applies?(
-             %Basket{
-               products: [
-                 %Product{code: "TSHIRT"},
-                 %Product{code: "TSHIRT"},
-                 %Product{code: "TSHIRT"}
-               ]
-             },
-             TwoForOne
-           )
-  end
-
-  test "should apply a Discount" do
-    assert Discounts.applies?(
-             %Basket{
-               products: [
-                 %Product{code: "TSHIRT"},
-                 %Product{code: "TSHIRT"},
-                 %Product{code: "TSHIRT"}
-               ]
-             },
-             Bulk
-           )
-
-    assert Discounts.applies?(
-             %Basket{
-               products: [
-                 %Product{code: "VOUCHER"},
-                 %Product{code: "VOUCHER"}
-               ]
-             },
-             TwoForOne
-           )
+    test "should apply TwoForOne discount" do
+      assert 100 ==
+               Discounts.apply(
+                 %Basket{
+                   products: [
+                     %Product{code: "VOUCHER", price: 100},
+                     %Product{code: "VOUCHER", price: 100}
+                   ]
+                 },
+                 TwoForOne
+               )
+    end
   end
 end
