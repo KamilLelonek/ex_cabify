a90d60f157d29036a3502492ff6eac2e2173563c
Kamil Lelonek <squixy.sln@gmail.com>
2018-08-24 01:46:27 +0200

    Bump versions

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule ExCabify.MixProject do
   def project do
     [
       app: :ex_cabify,
-      version: "0.1.0",
-      elixir: "~> 1.6",
+      version: "0.2.0",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
