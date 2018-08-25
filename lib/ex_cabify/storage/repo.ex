da2a8b416fec9169dc44130e3ccda8e3a4abff0c
Kamil Lelonek <squixy.sln@gmail.com>
2018-08-25 22:07:21 +0200

    Avoid reading products at compile time

--- lib/ex_cabify/storage/repo.ex
+++ lib/ex_cabify/storage/repo.ex
@@ -1,13 +1,15 @@
 defmodule ExCabify.Storage.Repo do
   alias ExCabify.Storage.Product
 
-  @products :ex_cabify
-            |> :code.priv_dir()
-            |> Path.join("products.json")
-            |> File.read!()
-            |> Poison.decode!(keys: :atoms, as: [%Product{}])
+  @file_path :ex_cabify
+             |> :code.priv_dir()
+             |> Path.join("products.json")
 
-  def all, do: @products
+  def all do
+    @file_path
+    |> File.read!()
+    |> Poison.decode!(keys: :atoms, as: [%Product{}])
+  end
 
   def one(code), do: Enum.find(all(), &(&1.code == code))
 end
