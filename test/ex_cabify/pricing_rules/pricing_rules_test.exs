97f67bdf69730510c9302e74d569676a1478abe8
Kamil Lelonek <squixy.sln@gmail.com>
2018-08-26 16:17:12 +0200

    Ters PricingRule application

--- test/ex_cabify/pricing_rules/pricing_rules_test.exs
+++ test/ex_cabify/pricing_rules/pricing_rules_test.exs
@@ -20,4 +20,27 @@ defmodule ExCabify.PricingRulesTest do
              ] = PricingRules.all()
     end
   end
+
+  describe "applies?(%{applicable_code: applicable_code, applicable_count: applicable_count}, codes)" do
+    test "should not apply a Pricing rule when there are no enough codes" do
+      pricing_rule = %{applicable_code: "TSHIRT", applicable_count: 2}
+      codes = ["TSHIRT"]
+
+      refute PricingRules.applies?(pricing_rule, codes)
+    end
+
+    test "should not apply a Pricing rule when there are no matching codes" do
+      pricing_rule = %{applicable_code: "VOUCHER", applicable_count: 1}
+      codes = ["TSHIRT", "TSHIRT"]
+
+      refute PricingRules.applies?(pricing_rule, codes)
+    end
+
+    test "should apply a Pricing rule" do
+      pricing_rule = %{applicable_code: "TSHIRT", applicable_count: 2}
+      codes = ["TSHIRT", "TSHIRT"]
+
+      assert PricingRules.applies?(pricing_rule, codes)
+    end
+  end
 end
