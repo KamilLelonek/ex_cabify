63d0f1cf4cd56de9b22013f542e7189494554232
Kamil Lelonek <squixy.sln@gmail.com>
2018-08-30 17:54:47 +0200

    Clean up Checkout interface

--- lib/ex_cabify/checkout.ex
+++ lib/ex_cabify/checkout.ex
@@ -1,9 +1,15 @@
 defmodule ExCabify.Checkout do
-  defstruct pricing_rules: [], codes: []
+  defstruct pricing_rules: [],
+            codes: []
+
+  alias ExCabify.{Storage.Repo, PricingRules}
 
   def new(pricing_rules \\ []),
     do: %__MODULE__{pricing_rules: pricing_rules}
 
   def scan(%__MODULE__{codes: codes} = checkout, code),
     do: %__MODULE__{checkout | codes: [code | codes]}
+
+  def total(%__MODULE__{codes: codes, pricing_rules: pricing_rules}),
+    do: Enum.reduce(pricing_rules, Repo.all_by_codes(codes), &PricingRules.refine/2)
 end
