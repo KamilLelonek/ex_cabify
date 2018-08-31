25a570569051457e5e6c96cc4c6d79271e7676ac
Kamil Lelonek <squixy.sln@gmail.com>
2018-08-31 22:49:08 +0200

    Provide Bulk pricing rule

--- lib/ex_cabify/pricing_rules/bulk_purchase.ex
+++ lib/ex_cabify/pricing_rules/bulk_purchase.ex
@@ -4,4 +4,14 @@ defmodule ExCabify.PricingRules.BulkPurchase do
   @enforce_keys ~w(applicable_count reduced_prize applicable_code)a
 
   defstruct @enforce_keys
+
+  alias ExCabify.{Storage.Product, PricingRules.Refinement}
+
+  defimpl Refinement do
+    def run(pricing_rule, products),
+      do: Enum.map(products, &reduce_price(&1, pricing_rule))
+
+    defp reduce_price(product, %{reduced_prize: reduced_prize}),
+      do: Product.update_price(product, reduced_prize)
+  end
 end
